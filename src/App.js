ef0c7a3ad46599565dafc7ca22f7f87995b5a965
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-01-11 13:58:45 -0200

    ✨ Adapting based on props

--- src/App.js
+++ src/App.js
@@ -4,26 +4,23 @@ import styled from 'styled-components';
 class App extends Component {
 
 	render() {
-		// Create a Title component that'll render an <h1> tag with some styles
-		const Title = styled.h1`
-			font-size: 1.5em;
-			text-align: center;
-			color: palevioletred;
-		`;
+		const Button = styled.button`
+			/* Adapt the colors based on primary prop */
+			background: ${props => props.primary ? "palevioletred" : "white"};
+			color: ${props => props.primary ? "white" : "palevioletred"};
 
-		// Create a Wrapper component that'll render a <section> tag with some styles
-		const Wrapper = styled.section`
-			padding: 4em;
-			background: papayawhip;
+			font-size: 1em;
+			margin: 1em;
+			padding: 0.25em 1em;
+			border: 2px solid palevioletred;
+			border-radius: 3px;
 		`;
-
 
 		return (
-			<Wrapper>
-				<Title>
-					Hello World!
-				</Title>
-			</Wrapper>
+			<React.Fragment>
+				<Button>Normal</Button>
+				<Button primary>Primary</Button>
+			</React.Fragment>
 		);
 	}
 }
