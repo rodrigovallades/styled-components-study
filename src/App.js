c67accc8f8ecf320bcda2ce83be772015768c340
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-01-11 16:19:31 -0200

    ✨ Add function theme

--- src/App.js
+++ src/App.js
@@ -3,38 +3,40 @@ import styled, { ThemeProvider } from 'styled-components';
 
 // Define our button, but with the use of props.theme this time
 const Button = styled.button`
+  color: ${props => props.theme.fg};
+  border: 2px solid ${props => props.theme.fg};
+  background: ${props => props.theme.bg};
+
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border-radius: 3px;
-
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.main};
-  border: 2px solid ${props => props.theme.main};
 `;
 
-// We are passing a default theme for Buttons that arent wrapped in the ThemeProvider
-Button.defaultProps = {
-  theme: {
-    main: "palevioletred"
-  }
-}
-
-// Define what props.theme will look like
+// Define our `fg` and `bg` on the theme
 const theme = {
-  main: "mediumseagreen"
+  fg: "palevioletred",
+  bg: "white"
 };
 
+// This theme swaps `fg` and `bg`
+const invertTheme = ({ fg, bg }) => ({
+  fg: bg,
+  bg: fg
+});
+
 class App extends Component {
+
   render() {
     return (
-			<React.Fragment>
-				<Button>Normal</Button>
-
-				<ThemeProvider theme={theme}>
-					<Button>Themed</Button>
-				</ThemeProvider>
-			</React.Fragment>
+			<ThemeProvider theme={theme}>
+				<React.Fragment>
+					<Button>Default Theme</Button>
+					<ThemeProvider theme={invertTheme}>
+						<Button>Inverted Theme</Button>
+					</ThemeProvider>
+				</React.Fragment>
+			</ThemeProvider>
     )
   }
 }
