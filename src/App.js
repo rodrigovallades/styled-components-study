cc58cedb0d65eecb6cad70fe5e78e3ad6b6574cc
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2019-01-11 16:14:52 -0200

    ✨ Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+
+// Define our button, but with the use of props.theme this time
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+
+  /* Color the border and text with theme.main */
+  color: ${props => props.theme.main};
+  border: 2px solid ${props => props.theme.main};
+`;
+
+// We are passing a default theme for Buttons that arent wrapped in the ThemeProvider
+Button.defaultProps = {
+  theme: {
+    main: "palevioletred"
+  }
+}
+
+// Define what props.theme will look like
+const theme = {
+  main: "mediumseagreen"
+};
 
 class App extends Component {
 
-	render() {
-		// Custom component
-		const Link = ({ className, children }) => (
-			<a className={className} href="/">
-				{children}
-			</a>
-		)
+	state = { count: 0 }
 
-		const StyledLink = styled(Link)`
-			color: palevioletred;
-			font-weight: bold;
-		`
-		return (
+  increment = () => this.setState({ count: this.state.count + 1 })
+  decrement = () => this.setState({ count: this.state.count - 1 })
+
+  render() {
+    return (
 			<React.Fragment>
-				<Link>Unstyled, boring link</Link>
-				<br />
-				<StyledLink>Styled, cool link</StyledLink>
+				<Button>Normal</Button>
+
+				<ThemeProvider theme={theme}>
+					<Button>Themed</Button>
+				</ThemeProvider>
 			</React.Fragment>
-		);
-	}
+    )
+  }
 }
 
 export default App;
